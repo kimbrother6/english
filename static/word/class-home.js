d6ce7a705cf153e9b3a23e92aea0620775c756d3
cubest_june <kim.hyung.june.08@gmail.com>
2021-08-17 16:26:57 +0900

    add word speak

--- static/word/class-home.js
+++ static/word/class-home.js
@@ -20,7 +20,28 @@ function loadClassWordsData() {
         //flip을 구현하기 위해서
         $('.flip-container .flipper').click(flip);
         $('.edit-btn').on('click', loadWordData);
+        $('.speaker_btn').on('click', speaker_btn_event)
       })
+}
+
+function speaker_btn_event(word) {
+  let id = $(this).attr('id');
+  fetch(`/${nowClass}/${id}/`)
+      .then((response) => response.json())
+      .then((result) => {
+        let word = JSON.parse(result.word)[0];
+        let wordFields = word.fields
+
+        speakWord(wordFields)
+      })
+}
+
+function speakWord(wordFields) {
+  let parameters = {
+    onend: () => { responsiveVoice.speak(`${wordFields.KO_word}`, 'Korean Male');}
+  }
+  responsiveVoice.speak(`${wordFields.EN_word}`, 'US English Male', parameters)
+  // responsiveVoice.speak(`${wordFields.KO_word}`, 'Korean Male')
 }
 
 function loadWordData() {
@@ -28,12 +49,29 @@ function loadWordData() {
   fetch(`/${nowClass}/${id}/`)
       .then((response) => response.json())
       .then((result) => {
-        //형변환 할때 iterable해야 하기 때문에 리스트형태로 만들었는데 데이터는 0번 인덱스에 있음
         let word = JSON.parse(result.word)[0];
         makeWordEditInput(word);
 
         $('html').click(event => {inputClickEvent(word, event)});
       })
+}
+
+function inputClickEvent(word, event) {
+  let id = word.pk;
+  let isClickEN_wordInput = $(event.target).hasClass(`EN_word_input-${id}`);
+  let isClickKO_wordInput = $(event.target).hasClass(`KO_word_input-${id}`);
+  let isNotClickInput = !(isClickEN_wordInput || isClickKO_wordInput);
+  let isNotDefaultWordInfoCard = !($(`#word-${id}`).html() === defaultWordInfoCardHtml)
+
+  if (isNotClickInput && isNotDefaultWordInfoCard) {
+    saveEditWordData(id)
+        .then((response) => response.json())
+        .then((result) => {
+          let modifiedWord = JSON.parse(result.word)[0]
+          loadDefaultWordInfoCardHtml(modifiedWord)
+          changeDefaultWordInfoCard(modifiedWord)
+        })
+  }
 }
 
 function makeFlipWordCard(words) {
@@ -53,14 +91,12 @@ function makeFlipWordCard(words) {
 function makeWordInfoCard(words) {
   let WordInfoCardHtml = '';
   for (word of words) {
-    WordInfoCardHtml += `<div class="word-card"><div class="word-text" id="word-${word.pk}"><div class="EN_word">${word.fields.EN_word}</div><div class="KO_word">${word.fields.KO_word}</div></div><div class="word-btns"><span class="star_btn"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16"><path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/></svg></span><span class="speaker_btn"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-volume-up-fill" viewBox="0 0 16 16"><path d="M11.536 14.01A8.473 8.473 0 0 0 14.026 8a8.473 8.473 0 0 0-2.49-6.01l-.708.707A7.476 7.476 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303l.708.707z"/><path d="M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.483 5.483 0 0 1 11.025 8a5.483 5.483 0 0 1-1.61 3.89l.706.706z"/><path d="M8.707 11.182A4.486 4.486 0 0 0 10.025 8a4.486 4.486 0 0 0-1.318-3.182L8 5.525A3.489 3.489 0 0 1 9.025 8 3.49 3.49 0 0 1 8 10.475l.707.707zM6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06z"/></svg></span><button class="edit-btn" id="${word.pk}"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16"><path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/></svg></button></div></div>`
+    WordInfoCardHtml += `<div class="word-card"><div class="word-text" id="word-${word.pk}"><div class="EN_word">${word.fields.EN_word}</div><div class="KO_word">${word.fields.KO_word}</div></div><div class="word-btns"><span class="star_btn"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-star-fill" viewBox="0 0 16 16"><path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/></svg></span><span class="speaker_btn" id="${word.pk}"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-volume-up-fill" viewBox="0 0 16 16"><path d="M11.536 14.01A8.473 8.473 0 0 0 14.026 8a8.473 8.473 0 0 0-2.49-6.01l-.708.707A7.476 7.476 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303l.708.707z"/><path d="M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.483 5.483 0 0 1 11.025 8a5.483 5.483 0 0 1-1.61 3.89l.706.706z"/><path d="M8.707 11.182A4.486 4.486 0 0 0 10.025 8a4.486 4.486 0 0 0-1.318-3.182L8 5.525A3.489 3.489 0 0 1 9.025 8 3.49 3.49 0 0 1 8 10.475l.707.707zM6.717 3.55A.5.5 0 0 1 7 4v8a.5.5 0 0 1-.812.39L3.825 10.5H1.5A.5.5 0 0 1 1 10V6a.5.5 0 0 1 .5-.5h2.325l2.363-1.89a.5.5 0 0 1 .529-.06z"/></svg></span><button class="edit-btn" id="${word.pk}"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16"><path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/></svg></button></div></div>`
   }
   WordInfoCardHtml += `<div class="word-create-or-delete-container"><a href="/${nowClass}/update" class="word-create-or-delete-a"><span class="word-create-or-delete-btn">단어 추가 / 삭제</span></a></div>`
   $('.word-card-list').html(`${WordInfoCardHtml}`)
 }
 
-
-//flip
 function flip() {
   $(this).closest('.flip-container').toggleClass('hover');
   $(this).css('transform, rotateY(180deg)');
@@ -70,25 +106,6 @@ let defaultWordInfoCardHtml;
 function loadDefaultWordInfoCardHtml(word) {
   let wordFields = word.fields;
   defaultWordInfoCardHtml = `<div class="EN_word">${wordFields.EN_word}</div><div class="KO_word">${wordFields.KO_word}</div>`
-}
-
-function inputClickEvent(word, event) {
-  let id = word.pk;
-  let isClickEN_wordInput = $(event.target).hasClass(`EN_word_input-${id}`);
-  let isClickKO_wordInput = $(event.target).hasClass(`KO_word_input-${id}`);
-  let isNotClickInput = !(isClickEN_wordInput || isClickKO_wordInput);
-  let isNotDefaultWordInfoCard = !($(`#word-${id}`).html() === defaultWordInfoCardHtml)
-  console.log(isNotDefaultWordInfoCard)
-
-  if (isNotClickInput && isNotDefaultWordInfoCard) {
-    saveEditWordData(id)
-        .then((response) => response.json())
-        .then((result) => {
-          let modifiedWord = JSON.parse(result.word)[0]
-          loadDefaultWordInfoCardHtml(modifiedWord)
-          changeDefaultWordInfoCard(modifiedWord)
-        })
-  }
 }
 
 function changeDefaultWordInfoCard(word) {
@@ -106,11 +123,10 @@ function makeWordEditInput(word) {
 
   $(`#word-${id}`).html(`
     <input type="hidden" name="csrfmiddlewaretoken" value="${csrftoken}">
-    <input type="text" name="EN_word" class="EN_word_input-${id}" autocomplete="off" value="${wordFields.EN_word}">
-    <input type="text" name="KO_word" class="KO_word_input-${id}" autocomplete="off" value="${wordFields.KO_word}">
+    <input type="text" name="EN_word" class="EN_word_input-${id} EN_word_input" autocomplete="off" value="${wordFields.EN_word}">
+    <input type="text" name="KO_word" class="KO_word_input-${id} KO_word_input" autocomplete="off" value="${wordFields.KO_word}">
     <input type="hidden" name="memorize" class="memorize-${id}" value="${wordFields.memorize}">
     <input type="hidden" name="Class" class="Class-${id}" value="${wordFields.Class}">
-    <button class="word-edit-submit" id="${id}">전송</button>
   `);
 }
 
