7712f2b14050b95ca4e7101151258ff71c42aaed
cubest_june <kim.hyung.june.08@gmail.com>
2021-09-13 22:09:52 +0900

    Resolved 404 error when reloading in React spa app

--- english/urls.py
+++ english/urls.py
@@ -20,13 +20,22 @@ from django.urls.conf import include
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 
+react_routes = [
+    'create_training_set',
+]
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    
+    #react app
     path('', TemplateView.as_view(template_name='index.html')),
+    path('create_training_set/', TemplateView.as_view(template_name='index.html')),
+
     path('data/', include('word.urls')),
     path('accounts/', include('accounts.urls')),
     #path(‘api/’, include(‘api.urls’)),
-
     ]
+
+
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
